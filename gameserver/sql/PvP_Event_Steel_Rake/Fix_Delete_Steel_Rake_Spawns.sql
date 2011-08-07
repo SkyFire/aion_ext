e61efd83ccb3cb36c2bbaef74849c48bae194722
MD <mike.dallas77@gmail.com>
2011-08-07 23:26:58 +0000

    Aion Extreme 2.5 1.) Description: Unfortunately the Aion Loot System is not so featured as I intended to make the loot, so we need to change the first Boss loot or change the portal entry requirements to be teleported to the final Boss. - Fixed First Boss droplist and adjusted portal item requirements. 1. Fix_Delete_Steel_Rake_Spawns.sql 2. update_Steel_Rake_Drops.sql

---
+++ gameserver/sql/PvP_Event_Steel_Rake/Fix_Delete_Steel_Rake_Spawns.sql
@@ -0,0 +1,17 @@
+﻿/*
+Navicat MySQL Data Transfer
+
+Source Server         : AE_Server25
+Source Server Version : 50141
+Source Host           : localhost:3306
+Source Database       : au_server_gs
+PvP Event Steel Rake  : @author Dallas Aion Extreme 2.5
+
+Target Server Type    : MYSQL
+Target Server Version : 50141
+File Encoding         : 65001
+
+Date: 2011-07-30 18:05:41
+*/
+
+DELETE FROM droplist WHERE mobid ='281443'
---
+++ gameserver/sql/PvP_Event_Steel_Rake/update_Steel_Rake_Drops.sql
@@ -0,0 +1,20 @@
+﻿/*
+Navicat MySQL Data Transfer
+
+Source Server         : AE_Server25
+Source Server Version : 50141
+Source Host           : localhost:3306
+Source Database       : au_server_gs
+PvP Event Steel Rake  : @author Dallas Aion Extreme 2.5
+
+Target Server Type    : MYSQL
+Target Server Version : 50141
+File Encoding         : 65001
+
+Date: 2011-07-30 18:05:41
+*/
+
+INSERT INTO `droplist` (`mobId`, `itemId`, `min`, `max`, `chance`) VALUES
+(281443, 185000037, 1, 1, 100),
+(281443, 168000112, 12, 12, 100),
+(281443, 162000066, 12, 12, 100);
